09521a22807052c5fcde94e7e04ba6e07c9e04a6
kelvo247 <B00119581@mytudublin.ie>
2025-04-24 16:45:45 +0100

    register models in admin

--- invoicing/admin.py
+++ invoicing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Staff, MenuItem, Invoice, InvoiceItem
+
+admin.site.register(Staff)
+admin.site.register(MenuItem)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
