3945055ef1da584f03c4712ef92a8e7ae6496eed
kelvo247 <B00119581@mytudublin.ie>
2025-04-24 21:19:37 +0100

    Tests added and working

--- invoicing/tests.py
+++ invoicing/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Staff, MenuItem, Invoice, InvoiceItem
 
@@ -9,9 +6,13 @@ class InvoiceTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='kelvo')
         self.staff = Staff.objects.create(user=self.user, role='cashier')
-        self.item = MenuItem.objects.create(name='Test Item', unit_price=4.50)
+        self.item = MenuItem.objects.create(name='Test Item', price=4.50)
         self.invoice = Invoice.objects.create(staff=self.staff)
-        self.invoice_item = InvoiceItem.objects.create(invoice=self.invoice, item=self.item, quantity=2)
+        self.invoice_item = InvoiceItem.objects.create(
+            invoice=self.invoice,
+            item=self.item,
+            quantity=2
+        )
 
     def test_invoice_total_amount_calculation(self):
         self.invoice.refresh_from_db()
@@ -22,6 +23,6 @@ class InvoiceTests(TestCase):
 
 class MenuItemTests(TestCase):
     def test_menu_item_creation(self):
-        item = MenuItem.objects.create(name='Burger', unit_price=5.99)
+        item = MenuItem.objects.create(name='Burger', price=5.99)
         self.assertEqual(item.name, 'Burger')
-        self.assertEqual(item.unit_price, 5.99)
+        self.assertEqual(item.price, 5.99)
